a162666bdc8c12860be3881971006d8600c092b8
John Larsson <john.larsson@scilifelab.se>
2015-11-27 08:23:23 +0100

    Added 'gene_id' to attribute column

--- scripts/prokkagff2gtf.sh
+++ scripts/prokkagff2gtf.sh
@@ -7,4 +7,4 @@ if [ "$infile" == "" ] ; then
     exit 0
 fi
 
-grep -v "#" $infile | grep "ID=" | cut -f1 -d ';' | sed 's/ID=//g' | cut -f1,4,5,7,9 |  awk -v OFS='\t' '{print $1,"PROKKA","CDS",$2,$3,".",$4,".",$5}'
+grep -v "#" $infile | grep "ID=" | cut -f1 -d ';' | sed 's/ID=//g' | cut -f1,4,5,7,9 |  awk -v OFS='\t' '{print $1,"PROKKA","CDS",$2,$3,".",$4,".","gene_id " $5}'
