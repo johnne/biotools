f2bcd9328a32bf0c76c1e504f3cf38d770c238e4
John Larsson <john.larsson@scilifelab.se>
2017-02-09 14:22:45 +0100

    Added importing of pandas

--- scripts/make_ncbi_treefile_mapfile.py
+++ scripts/make_ncbi_treefile_mapfile.py
@@ -2,13 +2,17 @@
 ###################################
 
 from ete3 import NCBITaxa
-ncbi = NCBITaxa()
-tree = ncbi.get_descendant_taxa(1,return_tree=True,intermediate_nodes=True)
-tree.write(outfile="ncbi.tre",format=8)
+import pandas as pd
+def main():
+    ncbi = NCBITaxa()
+    tree = ncbi.get_descendant_taxa(1,return_tree=True,intermediate_nodes=True)
+    tree.write(outfile="ncbi.tre",format=8)
+    
+    r = tree.get_tree_root()
+    taxids = [child.name for child in tree.get_children()]
+    taxids.append(r.name)
+    
+    ncbi_map = pd.DataFrame.from_dict(ncbi.get_taxid_translator(taxids),orient='index')
+    ncbi_map.to_csv("ncbi.map", sep="\t",header=False)
 
-r = tree.get_tree_root()
-taxids = [child.name for child in tree.get_children()]
-taxids.append(r.name)
-
-ncbi_map = pd.DataFrame.from_dict(ncbi.get_taxid_translator(taxids),orient='index')
-ncbi_map.to_csv("ncbi.map", sep="\t",header=False)
+if __name__ == '__main__': main()
