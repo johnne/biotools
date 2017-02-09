3bbfcffa0fbf7b97226b4f239617dad604daccf4
John Larsson <john.larsson@scilifelab.se>
2017-02-09 15:16:31 +0100

    use get_descendants to iterate all nodes in the tree

--- scripts/make_ncbi_treefile_mapfile.py
+++ scripts/make_ncbi_treefile_mapfile.py
@@ -9,7 +9,7 @@ def main():
     tree.write(outfile="ncbi.tre",format=8)
     
     r = tree.get_tree_root()
-    taxids = [child.name for child in tree.get_children()]
+    taxids = [child.name for child in tree.get_descendants()]
     taxids.append(r.name)
     
     ncbi_map = pd.DataFrame.from_dict(ncbi.get_taxid_translator(taxids),orient='index')
